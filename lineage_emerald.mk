f49e53034e74d12b78cb197ab0452b1f95aed867
shezan-tech <ariekechield22@gmail.com>
2025-06-01 00:32:46 +0700

    Update lineage_emerald.mk

--- lineage_emerald.mk
+++ lineage_emerald.mk
@@ -17,21 +17,18 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := emerald
-PRODUCT_NAME := Rising_emerald
+PRODUCT_NAME := lineage_emerald
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 2312FPCA6G
 TARGET_USES_PICO_GAPPS := true
-WITH_GAPPS := true
+WITH_GMS := true
 PRODUCT_MANUFACTURER := Xiaomi
-RISING_MAINTAINER="@androidme77"
 
 BUILD_FINGERPRINT :=POCO/emerald_p_id/emerald:14/UP1A.231005.007/V816.0.6.0.UNFIDXM:user/release-keys
 PRIVATE_BUILD_DESC="emerald_p_id-user 14 UP1A.231005.007 V816.0.6.0.UNFIDXM release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
        DeviceProduct=emerald \
-       RisingChipset="Mediatek Helio G99 Ultra" \
-       RisingMaintainer="Androidm77"
 
        
 PRODUCT_NO_CAMERA := false
